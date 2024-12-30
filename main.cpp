ae23f1ed47609e37a6df7f7640b864a3cf308400
Alya Qistina binti Awaluddin <alyaqistina@graduate.utm.my>
2024-12-30 18:15:48 +0800

    helo

--- main.cpp
+++ main.cpp
@@ -17,10 +17,27 @@ class Pizza{
     string itemName;
     string size;
     string set;
+    double price;
 
+<<<<<<< HEAD
+    public:
+
+    Pizza(string i, string n, double p):
+    itemID(i), itemName(n),price(p){}
+
+    void printDetails() const{
+
+        cout << "PIZZA SALES AND ORDER MANAGEMENT SYSTEM\n\n";
+
+        cout << "==MAIN MENU==\n\n";
+
+        cout << "[1] STAFF\n [2] CUSTOMER"
+    }
+=======
     private:
 
-helooo
+
+>>>>>>> 48d2be41e990777c7149b1f2a5a79ebc3d85701b
     
     
 

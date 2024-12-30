f89489adaa2a283ae65b690b72a2cc668bc176ce
Alya Qistina binti Awaluddin <alyaqistina@graduate.utm.my>
2024-12-30 18:02:29 +0800

    helo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+
 using namespace std;
 
 cout << "Hello World";
 
 cout << "kjaskaj";
 
+class Pizza{
+    private:
+    string itemID;
+    string itemName;
+    string size;
+    string set;
+
+    private:
+    
+    
+
+}
+
+
+
+
+
+
+
+system "pause";
 return 0;
 
 cout << "test 2";
